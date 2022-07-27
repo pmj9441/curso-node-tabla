ff5c3b0cd05574aa190f77b287a09662de7ae01b
Pablo <pablojara173@gmail.com>
2022-07-26 21:17:40 -0300

    Primer commit

---
+++ app.js
@@ -0,0 +1,27 @@
+
+const {crearArchivo} = require('./helpers/multiplicar');
+const argv = require ('./config/yargs');
+require('colors');
+
+
+console.clear();
+
+
+
+/*console.log(argv);
+console.log('base: yargs', argv.base);*/
+
+
+
+/*const [ , , arg3 = 'base=5' ] = process.argv;
+const [, base=5] = arg3.split('=');
+console.log(base);*/
+
+
+
+
+//const base = 5;
+
+crearArchivo(argv.b,argv.l,argv.h)
+    .then(nombreArchivo=>console.log(nombreArchivo.rainbow,'creado'))
+    .catch(err=>console.log(err));
---
+++ salida/info.md
@@ -0,0 +1,2 @@
+# Informacion 
+Esta carpeta contiene del producto final de la aplicacion
